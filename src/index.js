00d57bde24415a99cfef9d8593ef1b9b14657d97
Msouza <msdsantos96@gmail.com>
2022-02-09 07:04:17 -0300

    update

--- src/index.js
+++ src/index.js
@@ -96,8 +96,7 @@ app.post('/withdraw', verifyIFExixtsAccountCPF, (request, response) =>{
 
     const balance = getBalance(customer.statement);
 
-    console.log(balance)
-    console.log(amount)
+ 
 
     if(balance < amount){
         return response.status(400).json({ error: "Insufficient funds"})
